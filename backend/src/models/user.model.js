09baa14782a80660be4b6248943798a3ceeb3c7e
vikashgoswami30 <109417405+vikashgoswami30@users.noreply.github.com>
2025-12-15 13:29:42 +0530

    Updating in Controllers

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -51,10 +51,9 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
@@ -87,7 +86,5 @@ userSchema.methods.generateRefreshToken = function () {
     }
   );
 };
-
-
 
 export const User = mongoose.model("User", userSchema);
